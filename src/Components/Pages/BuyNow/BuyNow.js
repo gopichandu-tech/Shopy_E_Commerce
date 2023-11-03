ab704e2dc672aee188b2cbe7e7133b0172855f50
gopichandu-tech <70055677+gopichandu-tech@users.noreply.github.com>
2023-11-03 11:49:02 +0530

    buy-now page updated

--- src/Components/Pages/BuyNow/BuyNow.js
+++ src/Components/Pages/BuyNow/BuyNow.js
@@ -18,7 +18,7 @@ const BuyNow = () => {
         <div className='check-box'>
           <input type='checkbox' /><p>Protect My Details (optional)</p>
         </div>
-        {(signup && validateemail && validatephonenumber && validatepincode && validateusername) === false ? <div className='disable-button'><p className='fill-inputs'>Details Missing Or haven't SignUp</p> <button disabled>Place Order</button></div>:<div><p className='fill-inputs'>Details Saved and Details will be automatically deleated when you try to change and double click on place order if you purchased bebore place your order</p> <button className='apply-button' onClick={handleClick}>Place Order</button></div> }
+        {(signup && validateemail && validatephonenumber && validatepincode && validateusername) === false ? <div className='disable-button'><p className='fill-inputs'>Details Missing Or haven't SignUp</p> <button disabled>Place Order</button></div>:<div><p className='fill-inputs'>Details Saved and Details will be automatically deleated when you try to change, Note : Double click on place order button if you purchased before.</p> <button className='apply-button' onClick={handleClick}>Place Order</button></div> }
         {signup && click && validateemail && validatephonenumber && validatepincode && validateusername  ? <div className='placed-order'><p className='placed-order-text'>Order Placed Sucefully</p><Link to='/' className='placed-order-button' style={{textDecoration : "none"}} onClick={handleClick}>Return To Home</Link></div> : null}    
       </div>
   )
